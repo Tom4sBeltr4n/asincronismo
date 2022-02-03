18aaff34f366fd41d86f722d27ff93fe92a9e11b
Tom4sBeltr4n <tomasfelipe29@hotmail.com>
2022-02-03 16:12:49 -0500

    Class 7 NOT over, postponed till Monday, 7th February 2022. This topic is hard to chew on

--- examples.js
+++ examples.js
@@ -30,3 +30,24 @@ function printDate(dateNow)
 }
 
 date(printDate);
+
+//Promise example
+
+const somethingWillHappen = () =>
+{
+  //para comprender la sintaxis de la promesa: dentro de la función constructora "Promise()" se pone el parámetro Executor(). Executor es una función invoinvocable (callback) que permite inicializar la promesa, y que tiene los parámetros resolve() y reject(). En ambos casos son funciones que podemos usar dependiendo del resultado de la promesa; resolve() para éxito y reject() para fracaso. En este caso, usaremos una arrow function para inicializar a "Executor". También vale la pena notar que ponemos Promise como resultado para evitar que se ejecute de modo inmediato, sino cuando somethingWillHappen sea invocada, permitiendo controlar la promesa
+  return new Promise((resolve, reject) => {
+    if(false)
+    {
+      resolve('Hey!');
+    } else {
+      reject('Whooops!');
+    }
+  });
+};
+
+somethingWillHappen()
+  .then(response => console.log(response))//al invocar la promesa, debemos poner en la siguiente línea los métodos "then" y "catch". Ellos nos permitirán decirle a la promesa qué hacer cuando resuelva o rechace. Destaca que .then() puede (= es opcional) tener un segundo parámetro para cubrir el rechazo.
+  .catch(err => console.error(err));
+
+let i = 2;
